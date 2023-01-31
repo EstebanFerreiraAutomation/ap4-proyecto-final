f90a91aff6361ddf2c8390457a0f671a9847be56
Esteban Oscar Ferreira <ferreira_eo@hotmail.com>
2023-01-30 23:41:53 -0300

    Se agregó botón y métodos para cambiar el user

--- js/program.js
+++ js/program.js
@@ -1,17 +1,33 @@
+const specLetters = ["á", "é", "í", "ó", "ú", "ä", "ë", "ï", "ö", "ü"];
+
 const fillMockPerson = async () => {
   const response = await fetch("https://randomuser.me/api/?format=json");
-  const data = await response.json();
-  console.log(data);
-  document.getElementById("photo").src=data.results[0].picture.large
+  const data = await response.json();  
+  document.getElementById("photo").src = data.results[0].picture.large;
   document.getElementById(
     "name"
   ).innerHTML = `${data.results[0].name.first} ${data.results[0].name.last}`;
-  document.getElementById("email").innerHTML=data.results[0].email;
+  document.getElementById("email").innerHTML = data.results[0].email;
   document.getElementById(
     "website"
   ).innerHTML = `www.${data.results[0].name.first}${data.results[0].name.last}.com`;
-  document.getElementById("phoneNumber").innerHTML=`+${data.results[0].phone}`;
-
+  document.getElementById(
+    "phoneNumber"
+  ).innerHTML = `+${data.results[0].phone}`;
 };
+
+const changeProgressBars = () => {
+  let progressBars = document.getElementsByClassName("progress-bar");
+  for (let i = 0; i < progressBars.length; i++) {
+    let newValue = (Math.floor(Math.random() * 41) + 60).toString();
+    progressBars[i].setAttribute("aria-valuenow", newValue);
+    progressBars[i].style = `width: ${newValue}%`;
+  }
+};
+
+document.getElementById("nuevoUsuario").addEventListener("click", function () {
+  changeProgressBars();
+  fillMockPerson();
+});
 
 fillMockPerson();
